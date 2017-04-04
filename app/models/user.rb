5c4949ecce0b1a6ee5e6feb4ee16c5f8e558fb54
Brian Wilson <bountonw@gmail.com>
2017-04-04 15:36:09 +0000

    Add comments db model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :places
+  has_many :comments
+  
   validates :email, presence: true, uniqueness: true
 end
